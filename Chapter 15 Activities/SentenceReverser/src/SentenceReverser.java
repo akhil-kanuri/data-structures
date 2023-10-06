2d8956006cb3d390f4eaade3208351614f60a28d
akhil-kanuri <111461772+akhil-kanuri@users.noreply.github.com>
2023-10-06 13:51:49 -0500

    finished the reverse tester

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -15,14 +15,28 @@ public class SentenceReverser
     public static String reverse(String sentence)
     {
     	Scanner scanner = new Scanner(sentence);
-    	
-        // Complete this method. Use a Stack.
-        ...
+    	String reversed = "";
+        Stack<String> words = new Stack<>();
 
+        while(scanner.hasNext())
+        {
+            String next = scanner.next();
+            if(next.contains("."))
+            {
+                reversed = reversed + next.substring(0,1).toUpperCase() + next.substring(1,next.length()-1) + " ";
+                int stackSize = words.size();
+                for(int i = 0; i < stackSize-1; i++)
+                {
+                    reversed += words.pop() + " ";
+                }
+                reversed += words.pop().toLowerCase() + ". ";
+            }
+            else
+            {
+                words.push(next);
+            }
+        }
 
-
-
-
-
+        return reversed;
     }
 }
