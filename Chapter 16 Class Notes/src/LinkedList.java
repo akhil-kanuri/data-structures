0b839e4b4ac6e9878766f4bc83150640a4a3213f
akhil-kanuri <111461772+akhil-kanuri@users.noreply.github.com>
2023-10-16 14:12:37 -0500

    notes

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -8,11 +8,16 @@ import java.util.NoSuchElementException;
 */
 public class LinkedList
 {
-
+    //first variable refers to first node in the list
+    // if the node is empty first will be null
+    private Node first;
 
     /**
         Constructs an empty linked list.
     */
+    public LinkedList(){
+        this.first = null;
+    }
 
 
 
@@ -21,7 +26,12 @@ public class LinkedList
         Returns the first element in the linked list.
         @return the first element in the linked list
     */
-
+    public Object getFirst() {
+        if (this.first == null){
+            throw new NoSuchElementException();
+        }
+        return this.first.data;
+    }
 
 
 
@@ -29,7 +39,15 @@ public class LinkedList
         Removes the first element in the linked list.
         @return the removed element
     */
-
+    public Object removeFirst() {
+        if (this.first == null){
+            throw new NoSuchElementException();
+        }
+        
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
 
 
@@ -38,7 +56,12 @@ public class LinkedList
         Adds an element to the front of the linked list.
         @param element the element to add
     */
-
+    public void addFirst(Object element) {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = this.first; 
+        this.first = newNode; 
+    }
 
 
 
@@ -53,12 +76,25 @@ public class LinkedList
 
 
     //Class Node
+    //node is a static class because it doesn't need to acsess 
+    // anything in the Linked List
+    static class Node {
+        public Object data;
+        public Node next;
+        
+    }
 
-
-    class LinkedListIterator //implements ListIterator
+    class LinkedListIterator implements ListIterator
     {
       //private data
-
+        private Node position;
+        private Node previous;
+        private boolean isAfterNext;
+        public LinkedListIterator() {
+            position = null;
+            previous = null;
+            isAfterNext = false;
+        }
 
         /**
             Constructs an iterator that points to the front
